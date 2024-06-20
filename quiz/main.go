d5a032e39f997b3d80dab4701aa9f8edfc49b31c
jaskierrr <mr.xaski.2002@gmail.com>
2024-06-20 17:40:34 +0700

    added timer for all quiz, which parallel counts time

--- quiz/main.go
+++ quiz/main.go
@@ -9,9 +9,9 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
-var score int
 
 func readCsvFile(filepath string) [][]string {
 	file, err := os.Open(filepath)
@@ -29,33 +29,45 @@ func readCsvFile(filepath string) [][]string {
 }
 
 
-
-
 func main() {
-	csvFlag := flag.String("csv", "problems.csv", "help message for csv flag")
+	csvPath := flag.String("csv", "problems.csv", "choose file with quiz data")
+	timeLimit := flag.Int("limit", 30, "set time limit to quiz in seconds")
 	flag.Parse()
 
-	filePath, err := filepath.Abs(*csvFlag)
+	filePath, err := filepath.Abs(*csvPath)
 	if err != nil {
 		log.Fatalf("File not found: %v", err)
 	}
 
 	records := readCsvFile(filePath)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+	score := 0
+
+	labelloop:
 	for i := range records {
 		fmt.Println(records[i][0])
 
-		reader := bufio.NewReader(os.Stdin)
-		answear, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatalf("Unable read your answear")
-		}
+		answerCh := make(chan string)
 
-		answear = strings.TrimSuffix(answear, "\r\n")
+		go func () {
+			reader := bufio.NewReader(os.Stdin)
+			answer, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatalf("Unable read your answer")
+			}
+			answerCh <- strings.TrimSuffix(answer, "\r\n")
+		}()
 
-		if answear == records[i][1] {
-			score += 1
+		select {
+			case <- timer.C:
+				fmt.Printf("You answered %d questions out of %d correctly\n", score, len(records))
+				break labelloop
+			case answer := <- answerCh:
+				if answer == records[i][1] {
+					score++
+				}
 		}
 	}
-	fmt.Printf("You answered %d questions out of %d correctly\n", score, len(records))
 }
